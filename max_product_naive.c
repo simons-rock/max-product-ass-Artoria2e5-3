1b754da4dcbdacc8081a12aa221fff07a6daf0de
Mingye Wang <arthur200126@gmail.com>
2019-01-24 11:51:40 -0500

    Update max_product_naive.c

--- max_product_naive.c
+++ max_product_naive.c
@@ -1,10 +1,11 @@
 #include "max_product.h"
 
+// RULE: uint[] xs -> max(map((*), filter((!=), cartesian(xs, xs))))
 int max_pairwise_product_naive(int * arr, int size){
 	unsigned maxp = 0;
 	for (int i = 0; i < size; i++) {
 		// by symmerty; a[i] * a[j] == a[j] * a[i], so skip some
-		// note: skip self too
+		// and skip self too
 		for (int j = i + 1; j < size; j++) {
 			if (arr[i] == arr[j])
 				continue;
