ff2419f30ebfa5d63e234dd4dcd23975599adfbc
Mingye Wang <arthur200126@gmail.com>
2019-01-24 11:37:06 -0500

    Update max_product_fast.c

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,20 @@
 #include "max_product.h"
 
+// RULE: maxpair(int[] xs) -> (int a, int b) where a, b \in xs and a >= b >= xs \ {a,b}
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int max_a = 0, max_b = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > max_a) {
+			max_a = arr[i];
+			continue;
+		}
+		
+		if (arr[i] > max_b) {
+			max_a = arr[i];
+			continue;
+		}
+	}
+	
+	return max_a * max_b;
 }
 
