86657f23edfd001596d351d149f314ecf80a01f2
Mingye Wang <arthur200126@gmail.com>
2019-01-24 11:47:46 -0500

    FUCKING DISTINCT

--- max_product_naive.c
+++ max_product_naive.c
@@ -4,7 +4,10 @@ int max_pairwise_product_naive(int * arr, int size){
 	unsigned maxp = 0;
 	for (int i = 0; i < size; i++) {
 		// by symmerty; a[i] * a[j] == a[j] * a[i], so skip some
-		for (int j = i; j < size; j++) {
+		// note: skip self too
+		for (int j = i + 1; j < size; j++) {
+			if (arr[i] == arr[j])
+				continue;
 			unsigned p = arr[i] * arr[j];
 			if (p > maxp)
 				maxp = p;
