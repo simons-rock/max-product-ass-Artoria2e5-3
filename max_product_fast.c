65f74455ff335d6ec598539ecd3f37f895a979e1
Mingye Wang <arthur200126@gmail.com>
2019-01-24 11:45:21 -0500

    DISTINCT

--- max_product_fast.c
+++ max_product_fast.c
@@ -2,7 +2,7 @@
 
 // RULE:
 // int[] xs -> a * b where 	a, b = maxpair(xs)
-//			aka	a, b \in xs and a >= b >= xs \ {a,b}
+//			aka	a, b \in xs and a > b >= xs \ {a,b}
 int max_pairwise_product_fast(int *arr, int size){
 	unsigned max_a = 0, max_b = 0;
 	for (int i = 0; i < size; i++) {
@@ -11,8 +11,8 @@ int max_pairwise_product_fast(int *arr, int size){
 			continue;
 		}
 		
-		if (arr[i] > max_b) {
-			max_a = arr[i];
+		if (arr[i] != max_a && arr[i] > max_b) {
+			max_b = arr[i];
 			continue;
 		}
 	}
