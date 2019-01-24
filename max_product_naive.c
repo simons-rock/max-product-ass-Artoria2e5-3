751fa4588de43dca2871a07bbc69fb11abd74320
Mingye Wang <arthur200126@gmail.com>
2019-01-24 11:43:47 -0500

    Update max_product_naive.c

--- max_product_naive.c
+++ max_product_naive.c
@@ -1,11 +1,11 @@
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	int maxp = 0;
+	unsigned maxp = 0;
 	for (int i = 0; i < size; i++) {
 		// by symmerty; a[i] * a[j] == a[j] * a[i], so skip some
 		for (int j = i; j < size; j++) {
-			int p = arr[i] * arr[j];
+			unsigned p = arr[i] * arr[j];
 			if (p > maxp)
 				maxp = p;
 		}
